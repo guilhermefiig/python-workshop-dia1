44ebb480f08c3169dbc503c62e656f913ffc58ee
Guilherme Figueirêdo <127570959+guilhermefiig@users.noreply.github.com>
2023-08-30 15:27:58 -0300

    atualizaçoes de POO

--- 6POO/004.py
+++ 6POO/004.py
@@ -1,1 +1,53 @@
-...
+class FormaGeometrica:
+    def calcular_area(self, raio):
+        return 3.14 (raio ** 2)
+
+class Circulo(FormaGeometrica):
+    pass
+
+class Retangulo(FormaGeometrica):
+    def calcular_area(self, base, altura):
+        return base * altura
+
+class Quadrado(Retangulo):
+    pass
+
+circulo = Circulo()
+area = circulo.calcular_area(10)
+print(area)
+
+retangulo = Retangulo()
+area = retangulo.calcular_area(6,8)
+print(area)
+
+quadrado = Quadrado()
+area = quadrado.calcular_area(10,12)
+print(area)
+
+
+
+
+# class Garrafa:
+#     def init(self, tamanho, cor, marca):
+#         self.tamanho = tamanho
+#         self.cor = cor
+#         self.marca = marca
+
+#     def str(self):
+#         return f'tamanho: {self.tamanho}, marca:{self.marca}'
+
+
+# class GarrafaDeVidro(Garrafa):
+
+#     def quebrar(self):
+#         print('crack')
+
+
+# garrafa = Garrafa(150, 'azul', 'indaia')
+
+# garrafa_de_vidro = GarrafaDeVidro(200, 'transparente', 'vidro')
+
+
+# a = garrafa.str()
+# print(a)
+# print(garrafa_de_vidro.str())
--- 6POO/005.py
+++ 6POO/005.py
@@ -1,1 +1,29 @@
-...
+class Animal:
+    def __init__(self, animal, som, qtd_pata, habitat):
+        self.animal = animal
+        self.som = som
+        self.qtd_pata = qtd_pata
+        self.habitat = habitat
+        
+
+    def descricao(self):
+        print(f'\nO {self.animal} {self.som}, tem {self.qtd_pata} patas e o seu habitat é {self.habitat}.\n')
+
+class Cachorro(Animal):
+    pass
+
+class Gato(Animal):
+    pass
+
+class Cobra(Animal):
+    def descricao(self):
+        print(f'\nA {self.animal} {self.som}, tem {self.qtd_pata} e o seu habitat é {self.habitat}.\n')
+
+gato = Gato('gato', 'mia', 4, 'casa')
+cachorro = Cachorro('cachorro', 'late', 4, 'casa')
+
+gato.descricao()
+cachorro.descricao()
+
+cobra = Cobra('cobra', 'não faz som', 0, 'selva')
+cobra.descricao()
